e995ac2953d91ed1497b7135807420ef0c1a8679
ARYAVEER77 <77836395+ARYAVEER77@users.noreply.github.com>
2026-01-30 23:16:38 +0530

    Fix indentation in load_and_clean_data function

--- src/cleaning.py
+++ src/cleaning.py
@@ -4,12 +4,11 @@ def load_and_clean_data(file_path):
 
     df = pd.read_csv(file_path)
 
-
-    cols_to_use = [
-        'Year', 'Disaster Type', 'Country', 'Region',
-        'Start Month', 'Start Day',
-        'Total Deaths', 'Total Affected', "Total Damages ('000 US$)", 'Dis Mag Value'
-    ]
+cols_to_use = [
+    'Year', 'Disaster Type', 'Country', 'Region',
+    'Start Month', 'Start Day',
+    'Total Deaths', 'Total Affected', "Total Damages ('000 US$)", 'Dis Mag Value'
+]
     df = df[cols_to_use]
 
     df.dropna(subset=['Disaster Type', 'Total Deaths', 'Total Affected'], inplace=True)
@@ -18,3 +17,4 @@ def load_and_clean_data(file_path):
     df.fillna(0, inplace=True)
 
     return df
+
